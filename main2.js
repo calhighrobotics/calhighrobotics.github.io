b53fada72942b5317761af383a3c243c932e6571
calhighrobotics <calhighrobotics@gmail.com>
2017-06-19 15:28:31 -0700

    v(maybe)3.0(???)
    unsure if this will work

--- main2.js
+++ main2.js
@@ -1,19 +1,25 @@
 function passWord() {
-var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
-if (!pass1) 
-history.go(-1);
-if (pass1.toLowerCase() == "letmein") {
-alert('You Got it Right!');
-window.open('www.wikihow.com');
-break;
-} 
-testV+=1;
-var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    var testV = 1;
+    var pass1 = prompt('Please Enter Your Password', ' ');
+    while (testV < 3) {
+        if (!pass1)
+            history.go(-1);
+        if (pass1.toLowerCase() == "letmein") {
+            alert('You Got it Right!');
+            window.open('https://drive.google.com/open?id=0B5YYOq9dKJFucFg0anh1MDVyRkk');
+            break;
+        }
+        testV += 1;
+        var pass1 =
+            prompt('Access Denied - Password Incorrect, Please Try Again.', 'Password');
+    }
+    if (pass1.toLowerCase() != "password" & testV == 3)
+        history.go(-1);
+	    // very small annoyance
+	    alert("Sorry man, this is what you get for sticking your nose where it doesn't belong.")
+	    txt = "a";
+	    while (1) {
+	        txt = txt += "a"; //add as much as the browser can handle
+	    }
+    return " ";
 }
-if (pass1.toLowerCase()!="password" & testV ==3) 
-history.go(-1);
-return " ";
-} 
