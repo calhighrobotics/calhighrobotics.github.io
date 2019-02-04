1245eb7fd7af93d6298ebf4d913da22b3e94434c
Rahul S. Deshpande <rahulsachindeshpande@gmail.com>
2019-02-04 06:51:50 -0800

    whoo boye

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+function toggleNav() {
+    $("nav").toggleClass("togglednav");
+    $(".navlink").toggleClass("animated");
+    $(".navlink").toggleClass("slideInLeft");
+}
+
+function toggleTeamNav() {
+    toggleNav();
+    $("body").toggleClass("toggledteam");
+    $(".teamletter").toggleClass("animated");
+    $(".teamletter").toggleClass("slideInLeft");
+}
+
+currentYear = document.getElementById('currentYear');
+var date = new Date();
+var year = date.getFullYear();
+currentYear.innerHTML = year;
